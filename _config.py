5009686b82ee24261709f2e8f8a09bded99253d4
ELos385 <e.los18@imperial.ac.uk>
2021-08-04 10:54:42 +0100

    updated results structure

---
+++ _config.py
@@ -0,0 +1,7 @@
+"""
+    User specific configuration options.
+    Rename this file to config.py, and list it in .gitignore
+"""
+
+ROOT_DATA_FOLDER = '<YOUR DATA PATH>';
+FAILED_SAVES_FOLDER = '<YOUR failed saves FOLDER>';
---
+++ setup.py
@@ -0,0 +1,6 @@
+"""
+    Setting up analysis environment
+"""
+
+# Load some configuration variables etc. that are specific to this user
+from config import *
