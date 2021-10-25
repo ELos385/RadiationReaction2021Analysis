d5b393d6b38d00577eae07655b3878b672f059a6
ChrisArran <christopher.arran@york.ac.uk>
2021-10-25 11:26:47 +0100

    GammaProfile: Move calibration into a separate folder and add region of interest

--- calib/GammaProfile/GammaProfile.py
+++ calib/GammaProfile/GammaProfile.py
@@ -7,3 +7,7 @@
 # This is from Elias's calibration of 6th June 2021
 # Uses 2244 mm from North focus to gamma profile screen
 rad_per_px = 55.5e-6
+
+# Pulling corners of the diamond from first shot of run 9 on 6th June 2021
+roi = [(1024,1024),[(460,250),(160,560),(480,850),(780,530)]]
+
